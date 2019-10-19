871babbac2f2f72b8f2ce0dd167147d07f95399a
Artur Chernysh <chernysh.artur@gmail.com>
2019-10-19 14:38:07 +0300

    rollup init

---
+++ rollup.config.js
@@ -0,0 +1,14 @@
+import typescript from 'rollup-plugin-typescript2';
+
+export default [
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.min.js', format: 'iife' }],
+    plugins: [
+      typescript({
+        typescript: require('typescript'),
+        objectHashIgnoreUnknownHack: true,
+      }),
+    ],
+  },
+];
